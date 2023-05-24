672882b32e5f88fcae4eedf9e3bfea61e10939ed
unknown <nasyithmusyafa@gmail.com>
2023-05-24 16:16:10 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/views/pages/like.js
@@ -0,0 +1,23 @@
+import FavoriteMovieIdb from "../../data/favorite-movie-idb";
+import { createMovieItemTemplate } from "../templates/template-creator";
+import FavoriteMovieSearchView from "./liked-movies/favorite-movie-search-view";
+import FavoriteMovieSearchPresenter from "./liked-movies/favorite-movie-search-presenter";
+import FavoriteMovieShowPresenter from "./liked-movies/favorite-movie-show-presenter";
+
+const view = new FavoriteMovieSearchView();
+
+const Like = {
+  async render() {
+    return view.getTemplate();
+  },
+
+  async afterRender() {
+    new FavoriteMovieShowPresenter({ view, favoriteMovies: FavoriteMovieIdb });
+    new FavoriteMovieSearchPresenter({
+      view,
+      favoriteMovies: FavoriteMovieIdb,
+    });
+  },
+};
+
+export default Like;
